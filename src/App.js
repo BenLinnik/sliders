9933dc42589615d38e881005107db6e608852ef9
Benjamin Linnik <Benjamin@Linnik-IT.de>
2024-03-27 23:25:00 +0100

    plane through (160,7,30) (60,2,55) (140,5,55)

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
   const MIN_STUNDENSATZ = 0;
   const MAX_STUNDENSATZ = 120;
 
-  const FAKTOR_JAHRESAUFWAND = -8;
-  const FAKTOR_BETEILIGUNG = 495;
-  const FAKTOR_STUNDENSATZ = 72;
-  const KONSTANTE = -5190;
+  const FAKTOR_JAHRESAUFWAND = -3;  // x
+  const FAKTOR_BETEILIGUNG = 80;  // y
+  const FAKTOR_STUNDENSATZ = 4;  // z
+  const KONSTANTE = -200;
 
-  const [jahrestagesinvest, setJahrestagesinvest] = useState(150);
+  const [jahrestagesinvest, setJahrestagesinvest] = useState(140);
   const [gewinnbeteiligung, setGewinnbeteiligung] = useState(5);
-  const [stundensatz, setStundensatz] = useState(54.375);
+  const [stundensatz, setStundensatz] = useState(55);
 
   const [fixJahrestagesinvest, setFixJahrestagesinvest] = useState(false);
   const [fixGewinnbeteiligung, setFixGewinnbeteiligung] = useState(false);
